b80513bac23c7e2dbc81c803b5a433fc76bdcc50
+KZ <elmaestrodelsurvivalcraft@gmail.com>
2024-07-28 23:25:26 -0400

    initial version

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -41,6 +41,7 @@
 #include "gamemodes/ictf.h"
 #include "gamemodes/idm.h"
 #include "gamemodes/itdm.h"
+#include "gamemodes/iFreeze.h"
 #include "gamemodes/mod.h"
 #include "gamemodes/solofng.h"
 #include "gamemodes/zcatch.h"
@@ -3958,6 +3959,8 @@ void CGameContext::OnInit(const void *pPersistentData)
 		m_pController = new CGameControllerZcatch(this);
     else if(!str_comp_nocase(Config()->m_SvGametype, "bomb"))
         m_pController = new CGameControllerBOMB(this);
+    else if(!str_comp_nocase(Config()->m_SvGametype, "ifreeze+"))
+        m_pController = new CGameControllerIFreeze(this);
 	else if(!str_comp_nocase(Config()->m_SvGametype, "gdm"))
 		m_pController = new CGameControllerGDM(this);
 	else if(!str_comp_nocase(Config()->m_SvGametype, "idm"))
---
+++ src/game/server/gamemodes/iFreeze.cpp
@@ -0,0 +1,124 @@
+#include <game/server/entities/character.h>
+#include <game/server/player.h>
+
+#include "iFreeze.h"
+
+CGameControllerIFreeze::CGameControllerIFreeze(class CGameContext *pGameServer) :
+	CGameControllerTDM(pGameServer)
+{
+    m_GameFlags = GAMEFLAG_TEAMS | GAMEFLAG_FLAGS;
+    m_GameFlags_v7 = protocol7::GAMEFLAG_TEAMS;
+    m_pGameType = "iFreeze+";
+}
+
+CGameControllerIFreeze::~CGameControllerIFreeze() = default;
+
+void CGameControllerIFreeze::Tick()
+{
+	CGameControllerTDM::Tick();
+    
+    int Red = 0, Blue = 0, RedFr = 0, BlueFr = 0;
+
+    for(int i = 0; i < MAX_CLIENTS; i++)
+    {
+        if(GameServer()->m_apPlayers[i])
+        {
+            if(GameServer()->m_apPlayers[i]->GetTeam() == TEAM_BLUE)
+            {
+                Blue++;
+                if(GameServer()->GetPlayerChar(i) && GameServer()->GetPlayerChar(i)->GetCore().m_DeepFrozen)
+                    BlueFr++;
+            }
+            else if(GameServer()->m_apPlayers[i]->GetTeam() == TEAM_RED)
+            {
+                Red++;
+                if(GameServer()->GetPlayerChar(i) && GameServer()->GetPlayerChar(i)->GetCore().m_DeepFrozen)
+                    RedFr++;
+            }
+        }
+    }
+
+    bool BlueScored = RedFr >= Red && Red && RedFr;
+    if(BlueScored || (BlueFr >= Blue && Blue && BlueFr))
+    {
+        m_aTeamscore[(BlueScored) ? TEAM_BLUE : TEAM_RED]++;
+        ResetFrozenPlayer();
+        GameServer()->SendBroadcast((BlueScored) ? "Blue team scores" : "Red team scores", -1);
+        GameServer()->CreateSoundGlobal(SOUND_CTF_CAPTURE);
+    }
+}
+
+int CGameControllerIFreeze::OnCharacterDeath(class CCharacter *pVictim, class CPlayer *pKiller, int WeaponId)
+{
+	//CGameControllerInstagib::OnCharacterDeath(pVictim, pKiller, WeaponId);
+
+    /*
+	if(pKiller && WeaponId != WEAPON_GAME)
+	{
+		// do team scoring
+		if(pKiller == pVictim->GetPlayer() || pKiller->GetTeam() == pVictim->GetPlayer()->GetTeam())
+			m_aTeamscore[pKiller->GetTeam() & 1]--;
+		else
+			m_aTeamscore[pKiller->GetTeam() & 1]++;
+	}
+     */
+
+	// check score win condition
+	if((m_GameInfo.m_ScoreLimit > 0 && (m_aTeamscore[TEAM_RED] >= m_GameInfo.m_ScoreLimit || m_aTeamscore[TEAM_BLUE] >= m_GameInfo.m_ScoreLimit)) ||
+		(m_GameInfo.m_TimeLimit > 0 && (Server()->Tick() - m_GameStartTick) >= m_GameInfo.m_TimeLimit * Server()->TickSpeed() * 60))
+	{
+		if(m_SuddenDeath)
+		{
+			if(m_aTeamscore[TEAM_RED] / 100 != m_aTeamscore[TEAM_BLUE] / 100)
+			{
+				EndMatch();
+				return true;
+			}
+		}
+		else
+		{
+			if(m_aTeamscore[TEAM_RED] != m_aTeamscore[TEAM_BLUE])
+			{
+				EndMatch();
+				return true;
+			}
+			else
+				m_SuddenDeath = 1;
+		}
+	}
+	return false;
+}
+
+bool CGameControllerIFreeze::OnCharacterTakeDamage(vec2 &Force, int &Dmg, int &From, int &Weapon, CCharacter &Character)
+{
+    Dmg = 0;
+    CGameControllerTDM::OnCharacterTakeDamage(Force, Dmg, From, Weapon, Character);
+    if(GameServer()->m_apPlayers[From] == Character.GetPlayer())
+        return false;
+    if(GameServer()->m_apPlayers[From]->GetTeam() == Character.GetPlayer()->GetTeam())
+        return false;
+    if(GameServer()->m_apPlayers[From])
+    {
+        Character.Freeze();
+        Character.SetDeepFrozen(true);
+    }
+    return false;
+}
+
+void CGameControllerIFreeze::ResetFrozenPlayer()
+{
+    for(int i = 0; i < MAX_CLIENTS; i++)
+        if(GameServer()->m_apPlayers[i] && GameServer()->GetPlayerChar(i))
+        {
+            GameServer()->m_apPlayers[i]->KillCharacter(WEAPON_SELF);
+            GameServer()->m_apPlayers[i]->Respawn();
+        }
+}
+
+void CGameControllerIFreeze::OnCharacterSpawn(class CCharacter *pChr)
+{
+    CGameControllerTDM::OnCharacterSpawn(pChr);
+
+    // give default weapons
+    pChr->GiveWeapon(WEAPON_LASER, false, -1);
+}
