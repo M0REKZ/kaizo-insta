128880390ee16537d175cd3c397d981f6a9086cb
+KZ <elmaestrodelsurvivalcraft@gmail.com>
2024-10-27 20:06:31 -0300

    fix a bug

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -144,6 +144,13 @@ bool CCharacter::Spawn(CPlayer *pPlayer, vec2 Pos)
 
 void CCharacter::Destroy()
 {
+	if(m_HasBall) //+KZ
+	{
+		m_HasBall = false;
+		new CBall(&GameServer()->m_World, m_pPlayer->GetCid(), m_Pos, vec2(0,0));
+		GameServer()->CreateSound(m_Pos, SOUND_GRENADE_FIRE, TeamMask());
+	}
+	
 	GameServer()->m_World.m_Core.m_apCharacters[m_pPlayer->GetCid()] = 0;
 	m_Alive = false;
 	SetSolo(false);
