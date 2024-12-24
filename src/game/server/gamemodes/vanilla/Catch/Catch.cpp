ea327c6be888c235c59145594467856794a70cb1
+KZ <elmaestrodelsurvivalcraft@gmail.com>
2024-12-24 14:20:28 -0300

    untested

--- src/game/server/gamemodes/vanilla/Catch/Catch.cpp
+++ src/game/server/gamemodes/vanilla/Catch/Catch.cpp
@@ -22,7 +22,7 @@ CGameControllerCatch::CGameControllerCatch(class CGameContext *pGameServer) :
     Colors[1] = 255;
     for(int i = 2; i < MAX_CLIENTS; i++)
     {
-        Colors[i] = 1507072 + 1233 * i;
+        Colors[i] = 65280 + 1233000 * i;
     }
 }
 
