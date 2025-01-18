33d9373f978bc09e3f1bac2f08b3bb3031842774
+KZ <elmaestrodelsurvivalcraft@gmail.com>
2025-01-18 17:22:54 -0300

    Revert "untested bug fix"
    This reverts commit 0bfea07091ad9e43fe4bd570e2a084eb784f9ee4.

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -1028,10 +1028,6 @@ void CCharacter::Tick()
 			}
 		}
 	}
-	else
-	{
-		m_AirTicks = 5 * Server()->TickSpeed();
-	}
 	
 	if(!m_PrevInput.m_Hook && m_Input.m_Hook && !(m_Core.m_TriggeredEvents & COREEVENT_HOOK_ATTACH_PLAYER))
 	{
