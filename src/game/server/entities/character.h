bec6f795e345451bbc1e5122930b5afc2bee329c
+KZ <elmaestrodelsurvivalcraft@gmail.com>
2025-01-18 17:26:27 -0300

    Revert more changes

--- src/game/server/entities/character.h
+++ src/game/server/entities/character.h
@@ -277,8 +277,6 @@ private:
 	int m_AirDamageTick;
 	bool m_Invisible;
 	bool m_Water;
-	bool m_NoAir;
-	bool m_QuadWater;
 	int m_InvisibleShieldId;
 	int m_BallQueuedWeapon;
 	int m_BallReleaseTick;
